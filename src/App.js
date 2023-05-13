5cd998bfdd2d96370a06caf00008a412c814e679
lauralp92 <115834312+lauralp92@users.noreply.github.com>
2023-05-13 14:41:57 +0100

    added all files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Weather city="London" />
+      <footer>Coded by Laura Pitcher.</footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/WeatherData.js
@@ -0,0 +1,4 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Weather.css";
+import Forecast from "./ForecastData";
